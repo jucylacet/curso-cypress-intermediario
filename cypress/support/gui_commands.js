72ced5931c0f2d8d0febbdb4614ce93f19af88ce
Jucymara Lacet <jucymara.lacet@trampay.com>
2024-12-27 17:41:18 -0300

    second

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (
   user = Cypress.env('user_name'),
   password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
 ) => {
   const login = () => {
     cy.visit('/users/sign_in')
@@ -10,7 +11,22 @@ Cypress.Commands.add('login', (
     cy.get("[data-qa-selector='sign_in_button']").click()
   }
 
-  login()
+  const validate = () => {
+    cy.visit('/')
+    cy.location('pathname', { timeout: 1000 })
+      .should('not.eq', '/users/sign_in')
+  }
+
+  const options = {
+    cacheAcrossSpecs: true,
+    validate,
+  }
+
+  if (cacheSession) {
+    cy.session(user, login, options)
+  } else {
+    login()
+  }
 })
 
 Cypress.Commands.add('logout', () => {
@@ -25,4 +41,12 @@ Cypress.Commands.add('gui_createProject', project => {
   cy.get('#project_description').type(project.description)
   cy.get('.qa-initialize-with-readme-checkbox').check()
   cy.contains('Create project').click()
+})
+
+Cypress.Commands.add('gui_createIssue', issue => {
+  cy.visit(`/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+
+  cy.get('.qa-issuable-form-title').type(issue.title)
+  cy.get('.qa-issuable-form-description').type(issue.description)
+  cy.contains('Submit issue').click()
 })
